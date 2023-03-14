b64a79a05a08470c355fceaca3d06eb902758160
Yingyang Zhang <yingyang.zhang95@gmail.com>
2023-03-14 14:57:05 -0400

    added show method

--- app/controllers/furnitures_controller.rb
+++ app/controllers/furnitures_controller.rb
@@ -1,7 +1,13 @@
 class FurnituresController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    skip_before_action :authorized, only: [:index, :show]
 
     def index
         render json: Furniture.all
     end
+
+    def show
+        furniture = Furniture.find_by_id(params[:id])
+        render json: furniture
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :orders, only: [:create, :destroy]
   resources :carts, only: [:create]
   resources :cart_items, only: [:create, :update, :destroy]
-  resources :furnitures, only: [:index]
+  resources :furnitures, only: [:index, :show]
   get '/me', to: 'users#show'
   post '/login', to: 'auth#create'
   delete 'users/clear_bag/:id', to: 'users#clear_bag'
